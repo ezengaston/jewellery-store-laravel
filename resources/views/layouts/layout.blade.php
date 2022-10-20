9fde7ad939a4157b07c1cada2f872db64200631b
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-10-20 22:38:53 +0100

    Added nav

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <!-- Nab -->
+        <nav class="navbar navbar-expand-md bg-light">
+            <div class="container-xxl">
+                <a class="navbar-brand" href="/">
+                    <img src="logo.svg" alt="Logo" width="50" height="40" class="d-inline-block align-text-top" />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+                    <div class="navbar-nav">
+                        <a class="nav-link text-center px-4 fs-4" href="/">Home</a>
+                        <a class="nav-link text-center px-4 fs-4" href="/products">Products</a>
+                        <a class="nav-link d-md-none text-center fs-4" href="/login">Login</a>
+                        <a class="nav-link btn btn-secondary d-none d-md-inline text-center px-4 fs-4" href="/login">Login</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Content -->
+        @yield('content')
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    </head>
-    <body>
-
-    </body>
-</html>
+@section('content')
+    <div></div>
+@endsection
