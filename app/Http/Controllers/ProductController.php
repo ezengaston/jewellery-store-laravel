222b29d626cab1ea14473cf2788fc46ce29830a6
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-10-22 21:18:37 +0100

    Product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return null;
+    }
+
+    public function show()
+    {
+        return null;
+    }
+}
