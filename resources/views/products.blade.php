3da485eb383e969c828744f79d378e85f8f6d7c6
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-11-13 13:43:18 +0000

    Removed price filter

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,7 @@
         <div class="container-xxl text-center display-4">Our Products</div>
         <div class="container-xxl">
             <form class="row" action="/products" method="GET">
-                <div class="col-sm-2">
+                <div class="col-sm-3">
                     <div class="form-floating">
                         <select id="category" class="form-select">
                             @foreach ($categories as $category)
@@ -24,7 +24,7 @@
                         <label for="category" class="form-label">Category</label>
                     </div>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-3">
                     <div class="form-floating">
                         <select id="material" class="form-select">
                             @foreach ($materials as $material)
@@ -43,7 +43,7 @@
                         <label for="material" class="form-label">Material</label>
                     </div>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm-3">
                     <div class="form-floating">
                         <select id="gemstone" class="form-select">
                             @foreach ($gemstones as $gemstone)
@@ -62,36 +62,7 @@
                         <label for="gemstone" class="form-label">Gemstone</label>
                     </div>
                 </div>
-                <div class="col-sm-2">
-                    <div class="dropdown d-grid">
-                        <button class="btn btn-secondary dropdown-toggle btn-lg" type="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
-                            Price
-                        </button>
-                        <div class="dropdown-menu w-100">
-                            <div class="px-3">
-                                <div class="form-check dropdown-item remove-active-effect">
-                                    <input class="form-check-input" type="checkbox" value="" id="under500">
-                                    <label class="form-check-label" for="under500">
-                                        Under £500
-                                    </label>
-                                </div>
-                                <div class="form-check dropdown-item remove-active-effect">
-                                    <input class="form-check-input" type="checkbox" value="" id="500-1000">
-                                    <label class="form-check-label" for="500-1000">
-                                        £500-£1000
-                                    </label>
-                                </div>
-                                <div class="form-check dropdown-item remove-active-effect">
-                                    <input class="form-check-input" type="checkbox" value="" id="over1000">
-                                    <label class="form-check-label" for="over1000">
-                                        Over £1000
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
+                <div class="col-sm-3">
                     <div class="form-floating">
                         <input 
                             type="text" 
