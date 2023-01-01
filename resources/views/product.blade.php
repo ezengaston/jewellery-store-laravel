e6c55896c5741fb2d2d26adf7f5b8293394eff4b
Ezen <82026690+ezengaston@users.noreply.github.com>
2023-01-01 22:37:38 +0000

    Added add to basket button and input for quantity

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -12,11 +12,28 @@
                     <div class="lead mt-3 mb-1 fs-4">Description:</div>
                     <div class="fs-5">{{ $product->description }}</div>
                     <div class="lead mt-3 mb-1 fs-4">Details:</div>
-                    <ul class="fs-5">
+                    <ul class="fs-5 mb-4">
                         <li>Material - {{ $material->name }}</li>
                         <li>Gemstone - {{ $gemstone->name }}</li>
                         <li>Product number - {{ $product->id }}</li>
                     </ul>
+                    <form class="d-flex">
+                        <div class="form-floating">
+                            <input 
+                                type="number" 
+                                id="floatingQuantity" 
+                                class="form-control" 
+                                value="0"
+                                min="0"
+                            />
+                            <label 
+                                for="floatingQuantity"
+                            >
+                                Quantity
+                            </label>
+                        </div>
+                        <button class="btn btn-outline-primary ms-3">Add to basket</button>
+                    </form>
                 </div>
             </div>
         </div>
